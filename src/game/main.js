81bdebd51c039ea4cde392429703de34e637e319
Andy Gonzales <andreafgonzales@gmail.com>
2015-06-30 17:31:28 -0400

    initial commit, randomly generates tables (need to address corner case where they overlap); places player in the corner at 0,0

--- src/game/main.js
+++ src/game/main.js
@@ -1,3 +1,8 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+console.log('game made');
 
-console.log('im aive jasmine');
+//for more global(?) control of the game states, maybe consider refactoring to include a StateManager?
+game.state.add('at-mini-game', new AtDanceGame());
+game.state.start('at-mini-game');
+
+console.log('im aive andy ;)');
