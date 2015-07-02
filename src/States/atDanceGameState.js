757c3b17660b83b002af7b23626f4ccdee764727
Andy Gonzales <andreafgonzales@gmail.com>
2015-07-02 16:11:30 -0400

    added player, which follows the mouse cursor. Need to consider structuring of the code, esp. the player and the table

--- src/States/atDanceGameState.js
+++ src/States/atDanceGameState.js
@@ -1,10 +1,14 @@
 function AtDanceGame() {
+
 	//board dimensions
 	var BOARD_WIDTH, BOARD_HEIGHT;
 
 	//table
 	var tableCount;
 	var tableArray;
+
+	//player
+	var player;
 
 
 	function init() {
@@ -29,19 +33,20 @@ function AtDanceGame() {
 	console.log("loaded assets");
 
 	function create() {
-
 		//draws tables
 		for (var i = 0; i < tableCount; i ++) {
-			tableArray.push(new table());
+			tableArray.push(new createTable());
 			game.add.sprite(tableArray[i].x, tableArray[i].y, 'tableImg');
 		}
 
 		//draws table
-		game.add.sprite(player.x, player.y, 'playerImg');
+		player = game.add.sprite(createPlayer.x, createPlayer.y, 'playerImg');
 
 	}
 
 	function update() {
+		updatePlayer();
+
 
 	}
 
@@ -49,16 +54,32 @@ function AtDanceGame() {
 
 	}
 
-	function table(x, y) {
+	function createTable(x, y) {
 		this.x = game.rnd.realInRange(0, BOARD_WIDTH);
 		this.y = game.rnd.realInRange(0, BOARD_HEIGHT-25);  //25 buffers off-screen corner case, need to figure out how to get width
 	}
 
-	function player(x, y) {
+	function createPlayer(x, y) {
 		this.x = 10;
 		this.y = BOARD_HEIGHT/2 - 12.5; //25 being the height of the player
-		console.log(this.x, this.y);
-		this.speed = 3;
+	}
+
+	function updatePlayer() {
+
+		var speed = 10;
+		var yDistance = game.input.mousePointer.y - player.y;
+		var xDistance = game.input.mousePointer.x - player.x;
+
+		if (Math.sqrt(yDistance*yDistance +  xDistance*xDistance) < speed) {
+			player.x = game.input.mousePointer.x;
+			player.y = game.input.mousePointer.y;
+		} else {
+			var directionAngle = game.math.angleBetween(player.x, player.y, game.input.mousePointer.x, game.input.mousePointer.y);
+			player.x += Math.cos(directionAngle) * speed;
+			player.y += Math.sin(directionAngle) * speed;
+			player.angle = directionAngle * 180 / Math.PI;
+		}
+
 	}
 
 	return {
