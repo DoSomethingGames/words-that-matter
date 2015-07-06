c9d6f557ae246a9ab3023429ec4651d7e3edcedf
Andy Gonzales <andreafgonzales@gmail.com>
2015-07-06 13:38:08 -0400

    working on collision detection, not working

--- src/States/atDanceGameState.js
+++ src/States/atDanceGameState.js
@@ -5,11 +5,10 @@ function AtDanceGame() {
 
 	//table
 	var tableCount;
-	var tableArray;
+	var tables;
 
 	//player
 	var player;
-
 
 	function init() {
 
@@ -17,8 +16,8 @@ function AtDanceGame() {
 		BOARD_WIDTH = 800;
 
 		//table
-		tableArray = [];
-		tableCount = 5;
+		tables = null;
+		tableCount = 1;
 
 	}
 
@@ -30,54 +29,61 @@ function AtDanceGame() {
 
 	}
 
-	console.log("loaded assets");
-
 	function create() {
-		//draws tables
+
+		//enables Physics
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		//tables
+		tables = game.add.group();
+		tables.name = 'tables';
+		tables.enableBody = true;
+
 		for (var i = 0; i < tableCount; i ++) {
-			tableArray.push(new createTable());
-			game.add.sprite(tableArray[i].x, tableArray[i].y, 'tableImg');
+			table = tables.create(game.rnd.realInRange(0, BOARD_WIDTH), game.rnd.realInRange(0, BOARD_HEIGHT-25), 'tableImg');
+			table.body.collideWorldBounds = true;
+			table.body.immovable = true;
 		}
 
-		//draws table
-		player = game.add.sprite(createPlayer.x, createPlayer.y, 'playerImg');
+		//draws player
+		player = game.add.sprite(10, BOARD_HEIGHT/2 - 12.5, 'playerImg');
+		player.name = 'player';
+		game.physics.arcade.enable(player, Phaser.Physics.ARCADE);
+		player.body.allowGravity = false;
+		player.body.collideWorldBounds = true;
+		player.body.velocity = 10;
 
 	}
 
 	function update() {
-		updatePlayer();
 
+		if (!game.physics.arcade.collide(player, tables)) {
+			updatePlayer();
+		} else {
+			console.log('collision detected');
+		}
 
 	}
 
 	function shutdown() {
-
-	}
-
-	function createTable(x, y) {
-		this.x = game.rnd.realInRange(0, BOARD_WIDTH);
-		this.y = game.rnd.realInRange(0, BOARD_HEIGHT-25);  //25 buffers off-screen corner case, need to figure out how to get width
-	}
-
-	function createPlayer(x, y) {
-		this.x = 10;
-		this.y = BOARD_HEIGHT/2 - 12.5; //25 being the height of the player
+		player.destroy();
 	}
 
 	function updatePlayer() {
 
-		var speed = 10;
+		//mouse following
 		var yDistance = game.input.mousePointer.y - player.y;
 		var xDistance = game.input.mousePointer.x - player.x;
-
-		if (Math.sqrt(yDistance*yDistance +  xDistance*xDistance) < speed) {
+		if (Math.sqrt(yDistance*yDistance +  xDistance*xDistance) < player.body.velocity) {
+			//prevents jitter when sprite is at mouse location
 			player.x = game.input.mousePointer.x;
 			player.y = game.input.mousePointer.y;
 		} else {
+			//points sprite in direction of mouse and moves it based on speed
 			var directionAngle = game.math.angleBetween(player.x, player.y, game.input.mousePointer.x, game.input.mousePointer.y);
-			player.x += Math.cos(directionAngle) * speed;
-			player.y += Math.sin(directionAngle) * speed;
 			player.angle = directionAngle * 180 / Math.PI;
+			player.x += Math.cos(directionAngle) * player.body.velocity;
+			player.y += Math.sin(directionAngle) * player.body.velocity;
 		}
 
 	}
