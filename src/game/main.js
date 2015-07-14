7d683c3a0003337120f5835426d916f2b707bd2a
Andy Gonzales <andreafgonzales@gmail.com>
2015-07-14 14:03:16 -0400

    moved where mini-game is added to game

--- src/game/main.js
+++ src/game/main.js
@@ -1,8 +1,2 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 console.log('game made');
-
-//for more global(?) control of the game states, maybe consider refactoring to include a StateManager?
-game.state.add('at-mini-game', new AtDanceGame());
-game.state.start('at-mini-game');
-
-console.log('im aive andy ;)');
