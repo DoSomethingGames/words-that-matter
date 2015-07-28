0ec5e0a138fa07603a5573fa8850d8023b9007f9
jon uy <jonxuy@gmail.com>
2015-07-28 00:11:56 -0400

    Added neue styling and register form at end of the game - placeholder button asset and end needs to be replaced - actual user registration POST to northstar needs to be done

--- src/game/main.js
+++ src/game/main.js
@@ -1,2 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
+var registerSubmit = function() {
+  var fname;
+  var phone;
+
+  fname = $('#first_name').val();
+  phone = $('#phone').val();
+
+  console.log('@TODO: register user on northstar');
+};
